60a163c0fed8d29be9a0a163713fafcb75b5001d
mariliag <rocha.marilia@escolar.ifrn.edu.br>
2022-12-05 01:06:40 -0300

    aula04

--- Progressao/paginas/urls.py
+++ Progressao/paginas/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
 
-from .views import PaginaInicial
+from .views import PaginaInicial, SobreView
 
 urlpatterns = [
     # path('endereço/', MinhaView.as_view(), name='nome-da-url'),
     path('', PaginaInicial.as_view(), name='index'),
+    path('sobre/', SobreView.as_view(), name='sobre'),
 ]
--- Progressao/paginas/views.py
+++ Progressao/paginas/views.py
@@ -4,3 +4,6 @@ from django.views.generic import TemplateView
 # Create your views here.
 class PaginaInicial(TemplateView):
     template_name = 'paginas/index.html'
+
+class SobreView(TemplateView):
+    template_name = 'paginas/sobre.html'
